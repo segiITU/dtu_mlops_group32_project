c9f49324f16fb58d77d97725d18a22997619bb81
Arzu Burcu Guven <arzuburcuguven@gmail.com>
2025-01-21 18:11:23 +0100

    small fixes to train

--- src/dtu_mlops_group32_project/train.py
+++ src/dtu_mlops_group32_project/train.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
     parser.add_argument(
         '--checkpoint_dir',
         type=str,
-        default="models/checkpoints",
+        default=_PROJECT_ROOT + "models/checkpoints",
         help='Directory to save model checkpoints'
     )
     
