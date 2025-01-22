c4982596c0c4a2c12cc3116a478d0aabd496b400
Arzu Burcu Guven <arzuburcuguven@gmail.com>
2025-01-22 14:57:54 +0100

    correct test data

--- tests/unittests/test_data.py
+++ tests/unittests/test_data.py
@@ -12,45 +12,45 @@ def test_my_dataset():
     dataset = MyDataset("ccdv/pubmed-summarization")
 
     assert list(dataset[0].keys()) == ["train", "validation", "test"], "Dataset keys are incorrect"
-    assert list(dataset[0]["train"].keys()) == ["article", "summary"], "Dataset keys are incorrect"
-    assert list(dataset[0]["validation"].keys()) == ["article", "summary"], "Dataset keys are incorrect"
-    assert list(dataset[0]["test"].keys()) == ["article", "summary"], "Dataset keys are incorrect"
+    assert list(dataset[0]["train"].keys()) == ["article", "abstract"], "Dataset keys are incorrect"
+    assert list(dataset[0]["validation"].keys()) == ["article", "abstract"], "Dataset keys are incorrect"
+    assert list(dataset[0]["test"].keys()) == ["article", "abstract"], "Dataset keys are incorrect"
 
 
-def test_summary_length():
+def test_abstract_length():
     """Test that summaries are shorter than articles."""
 
     dataset = MyDataset("ccdv/pubmed-summarization")
 
     train_article = dataset[1]["train"]["article"]
-    train_summary = dataset[1]["train"]["summary"]
+    train_abstract = dataset[1]["train"]["abstract"]
     validation_article = dataset[1]["validation"]["article"]
-    validation_summary = dataset[1]["validation"]["summary"]
+    validation_abstract = dataset[1]["validation"]["abstract"]
     test_article = dataset[1]["test"]["article"]
-    test_summary = dataset[1]["test"]["summary"]
+    test_abstract = dataset[1]["test"]["abstract"]
     
-    assert len(train_summary) < len(train_article), "Summary in training data is not shorter than the article"
-    assert len(validation_summary) < len(validation_article), "Summary in validation data is not shorter than the article"
-    assert len(test_summary) < len(test_article), "Summary in test data is not shorter than the article"
+    assert len(train_abstract) < len(train_article), "abstract in training data is not shorter than the article"
+    assert len(validation_abstract) < len(validation_article), "abstract in validation data is not shorter than the article"
+    assert len(test_abstract) < len(test_article), "abstract in test data is not shorter than the article"
 
 
-def test_summary_content_overlap():
+def test_abstract_content_overlap():
     """Test content overlap between articles and summaries."""
 
     dataset = MyDataset("ccdv/pubmed-summarization")
 
     article = dataset[2]["train"]["article"]
-    summary = dataset[2]["train"]["summary"]
+    abstract = dataset[2]["train"]["abstract"]
     
     # Tokenizing into words
     article_words = article.split()
-    summary_words = summary.split()
+    abstract_words = abstract.split()
 
     # Generating n-grams for content overlap calculation
     article_unigrams = set(ngrams(article_words, 1))
-    summary_unigrams = set(ngrams(summary_words, 1))
+    abstract_unigrams = set(ngrams(abstract_words, 1))
 
     # Calculating overlap ratio
-    overlap = len(article_unigrams & summary_unigrams) / len(summary_unigrams)
+    overlap = len(article_unigrams & abstract_unigrams) / len(abstract_unigrams)
     
-    assert overlap > 0.1, "Content overlap between article and summary is too low"
+    assert overlap > 0.1, "Content overlap between article and abstract is too low"
