2888683f6d396da2752a8a3f7707cdbb684f3185
Arzu Burcu Guven <arzuburcuguven@gmail.com>
2025-01-24 13:29:47 +0100

    api tests

--- tests/integrationtests/test_api.py
+++ tests/integrationtests/test_api.py
@@ -2,7 +2,7 @@ from fastapi.testclient import TestClient
 from app.main import app
 client = TestClient(app)
 
-def test_read_root(model):
+def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Summarization API is running!"}
