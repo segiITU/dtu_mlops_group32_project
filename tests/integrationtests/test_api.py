c59b7734f2bb58bbda313fcaa3bd995613e63e12
Arzu Burcu Guven <arzuburcuguven@gmail.com>
2025-01-24 12:17:59 +0100

    paths again

--- tests/integrationtests/test_api.py
+++ tests/integrationtests/test_api.py
@@ -1,8 +1,10 @@
 from fastapi.testclient import TestClient
-from dtu_mlops_group32_project.main import app
+from app.main import app
 client = TestClient(app)
 
 def test_read_root(model):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Summarization API is running!"}
+
+
